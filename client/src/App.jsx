9a528837fa552ad6b625937d85a69898182a6bbc
Avinash  singh Patel <130283633+algo-avi@users.noreply.github.com>
2025-05-16 17:40:01 +0530

    Update App.jsx
    i change the spelling of Signup to SignUp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import { useState, useEffect } from "react"
 import Login from "./pages/Login"
-import Signup from "./pages/Signup"
+import Signup from "./pages/SignUp"
 import Dashboard from "./pages/Dashboard"
 import ProjectDetails from "./pages/ProjectDetails"
 import { AuthProvider } from "./context/AuthContext"
